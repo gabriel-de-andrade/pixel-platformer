6f7d52f47c3afa1e083c58245ad0a2e6307e2c9d
Gabriel de Andrade <101599406+gabriel-de-andrade@users.noreply.github.com>
2022-08-05 10:04:41 -0300

    Part 3 - Character animations and skins

--- Player.gd
+++ Player.gd
@@ -11,28 +11,49 @@ export(int) var ADITIONAL_FALL_GRAVITY = 2
 
 var velocity = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
+onready var animatedSprite = $AnimatedSprite
+
+func _ready() -> void:
+    animatedSprite.frames = load("res://PlayerGreenSkin.tres")
+
+func _physics_process(_delta: float) -> void:
     apply_gravity()
 
     var input = Vector2.ZERO
 
     input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
     if input.x == 0:
         apply_friction()
+        animatedSprite.animation = "idle"
     else:
         apply_acceleration(input.x)
+        animatedSprite.animation = "run"
+        if input.x > 0:
+            animatedSprite.flip_h = true
+        else:
+            animatedSprite.flip_h = false
 
     if is_on_floor():
         if Input.is_action_pressed("ui_up"):
             velocity.y = JUMP_FORCE
     else:
+        animatedSprite.animation = "jump"
+
         if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
             velocity.y = JUMP_RELEASE_FORCE
 
         if velocity.y > 0:
             velocity.y += ADITIONAL_FALL_GRAVITY
 
+    var was_in_air = not is_on_floor()
+
     velocity = move_and_slide(velocity, Vector2.UP)
+
+    var just_landed = was_in_air and is_on_floor()
+    if just_landed:
+        animatedSprite.animation = "run"
+        animatedSprite.frame = 1
 
 func apply_gravity() -> void:
     velocity.y += GRAVITY
