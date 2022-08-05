4fdc3baaef4385f1cd9f1bd7f96e0736ca740a45
Gabriel de Andrade <101599406+gabriel-de-andrade@users.noreply.github.com>
2022-08-05 13:49:31 -0300

    Part 5 - Spikes and camera

--- Player.gd
+++ Player.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
+class_name Player
 
 
-export(int) var JUMP_FORCE = -160
-export(int) var JUMP_RELEASE_FORCE = -40
-export(int) var MAX_SPEED = 75
-export(int) var ACCELETATION = 10
-export(int) var FRICTION = 15
-export(int) var GRAVITY = 5
-export(int) var ADITIONAL_FALL_GRAVITY = 2
+export(int) var JUMP_FORCE := -160
+export(int) var JUMP_RELEASE_FORCE := -40
+export(int) var MAX_SPEED := 75
+export(int) var ACCELETATION := 10
+export(int) var FRICTION := 15
+export(int) var GRAVITY := 5
+export(int) var ADITIONAL_FALL_GRAVITY := 2
 
-var velocity = Vector2.ZERO
+var velocity := Vector2.ZERO
 
-onready var animatedSprite = $AnimatedSprite
+onready var animatedSprite := $AnimatedSprite
 
 func _ready() -> void:
     animatedSprite.frames = load("res://PlayerGreenSkin.tres")
@@ -19,7 +20,7 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
     apply_gravity()
 
-    var input = Vector2.ZERO
+    var input := Vector2.ZERO
 
     input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 
@@ -46,11 +47,11 @@ func _physics_process(_delta: float) -> void:
         if velocity.y > 0:
             velocity.y += ADITIONAL_FALL_GRAVITY
 
-    var was_in_air = not is_on_floor()
+    var was_in_air := not is_on_floor()
 
     velocity = move_and_slide(velocity, Vector2.UP)
 
-    var just_landed = was_in_air and is_on_floor()
+    var just_landed := was_in_air and is_on_floor()
     if just_landed:
         animatedSprite.animation = "run"
         animatedSprite.frame = 1
---
+++ Spikes.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+
+func _on_Spikes_body_entered(body: Node) -> void:
+    if body is Player:
+        get_tree().reload_current_scene()
